cf504233aded7a49684abff23c349d802d7a6b8a
parikk <fariqhfebrian18@gmail.com>
2023-01-25 21:35:57 +0700

    Revert "lmi: Switch to GCamGOPrebuilt-V3"
    This reverts commit 32770e1829b958e3bf887f69aa3c05cedec6a0e9.

--- device.mk
+++ device.mk
@@ -159,7 +159,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    GCamGOPrebuilt-V3 \
+    GCamGOPrebuilt \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
